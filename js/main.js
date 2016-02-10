19209a5b72ecbf030d497df06576829a7eb17c9c
jonathan elbom <jb@jonnybomb.com>
2016-02-10 10:57:32 -0600

    thumbs working

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@
 	var $body = $('body');
 	var $callout = $('.callout')
 	var $shell = $('.nav-shell');
+	var $work = $('#work');
 	var photos;
 	var photo;
 	var _photos = {};
@@ -40,7 +41,9 @@
 	var collectionsId = 72157663848686246;
 	var navPaddingMax = 30;
 	var headerMarginBottom = $('header.navbar').css('margin-bottom');	
-
+	// define input event
+	var mouseEvent = Modernizr.touch ? 'touchend' : 'click';
+	
 	/**********************************************************
 	 *              scroll handler
 	 *********************************************************/
@@ -69,19 +72,19 @@
 	 $('header').on( mouseEvent, '.nav-item', function( e ) {
 		var $nav = $(this);
 		$('.nav-item').removeClass('selected');
-		var id = $nav.addClass('selected').data('sectionId');
 		var anchorId = $nav[0].hash.substr(1);
-		var photos = id === 'all' ? allPhotos : _.where(allPhotos, {photosetId: id})
 		e.preventDefault();
 		scrollToId( anchorId );
+		// var id = $nav.addClass('selected').data('sectionId');
+		// var photos = id === 'all' ? allPhotos : _.where(allPhotos, {photosetId: id})
 	});
 
 	/**********************************************************
 	 *                 thumbs handler
 	 *********************************************************/
-	$main.on( mouseEvent, '.thumb', function() {
+	$work.on( mouseEvent, '.thumb', function() {
 		if ( !scrolling ) {
-			showFullImage( findPhotoFromCurPhotos(  $(this).closest('div').data('photoId') ) );
+			showFullImage( findPhotoFromAllPhotos(  $(this).closest('div').data('photoId') ) );
 		}
 		scrolling = false;
 	}).on( 'touchmove', '.thumb', function() {
@@ -99,8 +102,6 @@
 	  .end()
 	  	.find('a[href="'+id+'"]')
 	  		.addClass('selected');
-	  console.log('activate.bs.scrollspy, id:', id);
-
 	});
 
 	/**********************************************************
@@ -150,7 +151,8 @@
 	// callout position - TODO: without javascript
 	$callout.css('top', 'calc(50% - '+$callout.outerHeight()/2+'px)'); 
 	$loader.hide();
-	$(window).scroll( 0 );
+	//$(window).scroll( 0 );
+	$('html,body').scrollTop( 0 )
 	// device detection
 	if ( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
 	    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
@@ -162,8 +164,6 @@
 			updateRowHeight();
 		});
 	}
-	// define input event
-	var mouseEvent = Modernizr.touch ? 'touchend' : 'click';
 	// kickoff site (get user id)
 	getUserId( clientUsername );
 	
@@ -171,15 +171,16 @@
 	/**********************************************************
 	 *                 methods
 	 *********************************************************/
+	
 	function scrollToId( id ){
 	    var $elem = $('#'+id);
 	    var padding = $('header').outerHeight();
-	    console.log('scrollToId'
-	    	,'id:',id
-	    	,'padding:',padding
-	    	,'$elem.offset():',$elem.offset()
+	    // console.log('scrollToId'
+	    // 	,'id:',id
+	    // 	,'padding:',padding
+	    // 	,'$elem.offset():',$elem.offset()
 
-	    )
+	    // )
 	    $('html,body').animate({scrollTop: $elem.offset().top-padding},500);
 	}
 
@@ -265,8 +266,9 @@
 
 	}
 
-	function findPhotoFromCurPhotos( photoId ) {
-		photo = _.findWhere( curPhotos, {id: photoId.toString()} );
+	function findPhotoFromAllPhotos( photoId ) {
+		photo = _.findWhere( allPhotos, {id: photoId.toString()} );
+		curPhotos = _photos[ photo.photosetId ];
 		return photo;
 	}
 	function findNextPhotoFromCurPhotos() {
@@ -281,23 +283,23 @@
 		photo = curPhotos[ idx ];
 		return photo;
 	}
-	function findPhotoFromSet( photoId, photosetId ) {
-		photos = photosets[ photosetId ].photoset.photo;
-		photo = _.findWhere( photos, {id: photoId.toString()} );
-		return photo;
-	}
-	function findNextPhotoFromSet() {
-		var idx = photos.indexOf( photo );
-		idx = idx === photos.length - 1 ? 0 : idx + 1;
-		photo = photos[ idx ];
-		return photo;
-	}
-	function findPrevPhotoFromSet() {
-		var idx = photos.indexOf( photo );
-		idx = idx ===  0 ? photos.length - 1 : idx - 1;
-		photo = photos[ idx ];
-		return photo;
-	}
+	// function findPhotoFromSet( photoId, photosetId ) {
+	// 	photos = photosets[ photosetId ].photoset.photo;
+	// 	photo = _.findWhere( photos, {id: photoId.toString()} );
+	// 	return photo;
+	// }
+	// function findNextPhotoFromSet() {
+	// 	var idx = photos.indexOf( photo );
+	// 	idx = idx === photos.length - 1 ? 0 : idx + 1;
+	// 	photo = photos[ idx ];
+	// 	return photo;
+	// }
+	// function findPrevPhotoFromSet() {
+	// 	var idx = photos.indexOf( photo );
+	// 	idx = idx ===  0 ? photos.length - 1 : idx - 1;
+	// 	photo = photos[ idx ];
+	// 	return photo;
+	// }
 
 	function getImgSrc( farm, server, id, secret, size) {
 		return 'http://farm' + farm + '.static.flickr.com/' + server + '/' + id + '_' + secret + '_' + size + '.jpg';
@@ -308,7 +310,6 @@
 	}
 	function initGrid( photosetId ) {
 		var $job = $('.work__job[data-section-id="'+photosetId+'"] .js-work__job__thumbs'); // $('.section')
-		console.log('$job:',$job);
 		// if ( hasGrid ) {
 		// 	$job.justifiedGallery('destroy');
 		// 	$job.justifiedGallery().off();
@@ -395,7 +396,7 @@
 	}
 	// when individual photoset is return from flickr
 	function onGetPhotoset(data) {
-		console.log('onGetPhotoset, data:', data);
+		//console.log('onGetPhotoset, data:', data);
 		var photoset = data.photoset;
 		photosets[ photoset.id ].photoset = photoset;
 		photosetsLoaded++;
@@ -403,24 +404,18 @@
 			photo.photosetId = data.photoset.id;
 			_photos[ photoset.id ].push( photo );
 			//console.log('_photos[',photoset.id,']:',_photos[ photoset.id ]);
-			//allPhotos.push( photo );
+			allPhotos.push( photo );
 		});
 		
 
+		// add all photos for this photoset
 		addPhotos( _photos[ photoset.id ] );
+		// init grid for this photoset
 		initGrid( photoset.id );
-		console.log('photoset:',photoset);
-		// if ( numPhotosets > 0 && photosetsLoaded === numPhotosets ) {
-		// 	addPhotos( allPhotos );
-		// 	initGrid();
-
-		// 	// console.log('init grid');
-		// }
-
 	}
 	function addPhotos( photos ) {
-		curPhotos = _.shuffle(photos);
-		_.each( curPhotos, addPhoto );
+		//curPhotos = _.shuffle(photos);
+		_.each( _.shuffle(photos), addPhoto );
 		
 		// // $('.section').on('click', '.thumb', function() {
 		// 	var photo = curPhotos.findWhere({});
@@ -451,9 +446,7 @@
 
 	// called when photosets obtained, either from collection for from user
 	function onPhotosetsObtained( sets ) {
-		console.log('onPhotosetsObtained'
-			,', sets:',sets
-		);
+		//console.log('onPhotosetsObtained, sets:',sets);
 		var $nav = $('#nav-scroll > .navbar-right');
 		var $work = $('.work');
 		$imageHolder = $( templates.section.concat() ).appendTo( $main );
@@ -488,7 +481,7 @@
 	// when a collection tree is returned from flickr
 	function onGetCollectionTree(data) {
 		var collection = _.findWhere( data.collections.collection, {id: pizzaOvenCollectionId} );
-		console.log('collection:',collection)
+		//console.log('collection:',collection)
 		// populate about section with colletion title and description
 		$('.js-about__title').text( collection.title );
 		$('.js-about__description').text( collection.description );
